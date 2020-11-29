2dc9d3bc2e96aa76a4d3551254506c72e62c0012
Edward <e.arechwa@gmail.com>
2020-11-29 19:42:39 +0100

    integrate ts with functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const App = () => {}
+type TitleType = {
+  title: string
+}
+
+const Title = ({title}: TitleType) => <h1>{title}</h1>
+
+const App = () => <Title title={'Hello World'} />
 
 export default App;
