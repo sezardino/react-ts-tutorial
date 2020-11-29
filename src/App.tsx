97499d2fe319494cd219c6a742c8d0a00f479601
Edward <e.arechwa@gmail.com>
2020-11-29 20:29:25 +0100

    intagrate events with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 type CounterProps = {
-  title?: string
-}
+	title?: string;
+};
 
 type CounterState = {
-  count: number
+	count: number;
+};
+
+class Counter extends Component<CounterProps, CounterState> {
+	state = {
+		count: 0,
+	};
+
+	componentDidMount(): void {}
+
+	shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState): boolean {
+		return true;
+	}
+
+	static getDerivedStateFromProps(props: CounterProps, state: CounterState): null | CounterState {
+		return false ? {count: 2} : null;
+	}
+
+	clickHandler = (evt: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+    console.log(evt);
+		this.setState(({count}) => ({count: ++count}));
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.count}</h1>
+				<button onClick={this.clickHandler}>+</button>
+				<a href="#id" onClick={this.clickHandler}>+</a>
+			</div>
+		);
+	}
 }
 
-class Counter extends Component <CounterProps, CounterState> {
-  state = {
-    count: 0
+class Form extends Component <{}, {}> {
+
+  inputHandler = (evt: React.FocusEvent) => {
+    console.log(evt.target);
   }
 
-  componentDidMount():void {}
-
-  shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState):boolean {
-    return true;
-  }
-
-  static getDerivedStateFromProps(props: CounterProps, state: CounterState): null | CounterState {
-    return false ? {count: 2} : null
-  }
-
-  clickHandler = () => {
-    this.setState(({count}) => ({count: ++count}))
+  submitHandler = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    console.log('Submitted');
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.clickHandler}>+</button>
-      </div>
+      <form onSubmit={this.submitHandler}>
+        <label>
+          <input onFocus={this.inputHandler} type="text" name="text"/>
+        </label>
+        <button type="submit">submit</button>
+      </form>
     )
   }
 }
 
-const App = () => <Counter />
+const App = () => (
+	<>
+		<Counter />
+    <Form />
+	</>
+);
 
 export default App;
