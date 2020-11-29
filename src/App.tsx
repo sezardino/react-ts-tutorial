a99fc4d89be061cc0774678e8b868156eb897bce
Edward <e.arechwa@gmail.com>
2020-11-29 20:12:35 +0100

    intagrate ts with class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-type TitleType = {
-  title: string
+type CounterProps = {
+  title?: string
 }
 
-const Title = ({title}: TitleType) => <h1>{title}</h1>
+type CounterState = {
+  count: number
+}
 
-const App = () => <Title title={'Hello World'} />
+class Counter extends Component <CounterProps, CounterState> {
+  state = {
+    count: 0
+  }
+
+  componentDidMount():void {}
+
+  shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState):boolean {
+    return true;
+  }
+
+  static getDerivedStateFromProps(props: CounterProps, state: CounterState): null | CounterState {
+    return false ? {count: 2} : null
+  }
+
+  clickHandler = () => {
+    this.setState(({count}) => ({count: ++count}))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.clickHandler}>+</button>
+      </div>
+    )
+  }
+}
+
+const App = () => <Counter />
 
 export default App;
